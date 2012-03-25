f5d6ecdcb999b3398ca623a492c268cb81934db9
Jeremiah Malina <jmalina327@gmail.com>
2012-03-25 07:43:16 -0400

    Frontend

--- newsapp/static/js/loadImages.js
+++ newsapp/static/js/loadImages.js
@@ -2,12 +2,14 @@
 
 $(document).ready(function(){
 
-   $('article').click(function(event){
-   		if ($(this).children("section").attr('class') == "masonryImages_hide"){
-   			$(this).children("section").attr('class', 'masonryImages_show');}
-   		else 
-   			$(this).children("section").attr('class', 'masonryImages_hide');
-   });
+   // $('article').click(function(event){
+   // 		if ($(this).children("section").attr('class') == "masonryImages_hide"){
+   // 			$(this).children("section").attr('class', 'masonryImages_show');}
+   // 		else 
+   // 			$(this).children("section").attr('class', 'masonryImages_hide');
+   // });
+
+	
 
 
 });
