e5297f387edad65edfd46714387a9711fc37a87f
Cody Coats <codycoats@me.com>
2012-03-25 06:48:30 -0400

    new settings for static css

---
+++ newsapp/static/js/loadImages.js
@@ -0,0 +1,13 @@
+/* loadImages.js */
+
+$(document).ready(function(){
+
+   $('article').click(function(event){
+   		if ($(this).children("section").attr('class') == "masonryImages_hide"){
+   			$(this).children("section").attr('class', 'masonryImages_show');}
+   		else 
+   			$(this).children("section").attr('class', 'masonryImages_hide');
+   });
+
+
+});
