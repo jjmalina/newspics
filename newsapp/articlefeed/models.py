9586b8393519c775516acca2d233b3518d0b3690
Jeremiah Malina <jmalina327@gmail.com>
2012-03-24 22:17:25 -0400

    Models and migrations

--- newsapp/articlefeed/models.py
+++ newsapp/articlefeed/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    image_url = models.URLField(
+        verify_exists=False,
+        max_length=255,
+        unique=True,
+        help_text='The full, canonical, non-shortened URL for the image.'
+    )
+    image_height = models.IntegerField(blank=True, null=True)
+    image_width = models.IntegerField(blank=True, null=True)
+    date_downloaded = models.DateTimeField(auto_now_add=True)
+
+
+class Article(models.Model):
+    url = models.URLField(
+        verify_exists=False,
+        max_length=255,
+        unique=True,
+        help_text='The full, canonical, non-shortened URL for the article.'
+    )
+    title = models.CharField(max_length=255, blank=True, null=True)
+    author_name = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        help_text="Author name as given by an external API."
+    )
+    content = models.TextField(
+        blank=True,
+        null=True,
+        help_text="Original content for the article"
+    )
+    pub_date = models.DateTimeField()
+    date_downloaded = models.DateTimeField(auto_now_add=True)
+    images = models.ManyToManyField(Image, through='Topic', related_name='articles')
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=40, blank=True, null=True)
+    article = models.ForeignKey(Article, related_name='topics')
+    image = models.ForeignKey(Image, related_name='topics')
+    date_downloaded = models.DateTimeField(auto_now_add=True)
