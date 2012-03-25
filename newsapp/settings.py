b0c8ec2113dfc4cf56c9a4d666859cc84b49dae4
Jeremiah Malina <jmalina327@gmail.com>
2012-03-24 21:05:53 -0400

    settings and articlefeed app

--- newsapp/settings.py
+++ newsapp/settings.py
@@ -125,6 +125,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    PROJECT_ROOT + '/templates'
 )
 
 INSTALLED_APPS = (
@@ -137,6 +138,7 @@ INSTALLED_APPS = (
     'south',
     'djsupervisor',
     'djcelery',
+    'articlefeed',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
